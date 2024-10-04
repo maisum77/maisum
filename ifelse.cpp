5769808cd43a9be2fef4075158a0b43f8cda3acc
maisumabbas03@gmail.com <maisumabbas03@gmail.com>
2024-10-04 22:05:00 +0500

    ifelse

---
+++ ifelse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int a ;
+    cout << "enter integer = \n";
+    cin >> a ;
+    if ( a % 2 == 0 )
+    {
+        cout << " number is even " ;
+
+    }
+    else 
+    cout << "number is odd ";
+clrscr ()
+return 0 ;
+
+}
