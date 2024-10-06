85e30c220bbd0be4ddb99a0a9534310246a5c956
maisumabbas03@gmail.com <maisumabbas03@gmail.com>
2024-10-06 14:22:38 +0500

    area

---
+++ areacircle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    float radius ;
+    cout << "enter radius = " << endl ;
+    cin >> radius ;
+    int area = radius * radius * 3.14 ;
+    cout << "area of circle is = " << area ;
+    return 0 ; 
+}
