f8506cfe9f2fcdfbf1b465cc9116341beb4cc1e1
Maisum abbas <104243145+maisum77@users.noreply.github.com>
2024-10-24 09:52:45 +0500

    Add files via upload

---
+++ nested if.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+	int x = 10 ;
+	int y = 5 ;
+	if (x>5)
+	{
+		if (y>2)
+		{
+			cout << "both conditions are true !";
+		}
+		else
+		 {
+			cout << "x is grreater then 5 and y is smaller then 2 ";
+			
+		}
+	}
+	else 
+	{
+		cout << "x is not greater then 5 " ;
+	}
+	return 0 ;
+}
