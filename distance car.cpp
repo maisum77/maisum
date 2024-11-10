cc4888bf205ea290a21c93149c90a71ef219aef0
Maisum abbas <maisumabbas03@gmail.com>
2024-11-10 15:37:52 +0500

    distance car

---
+++ distance car.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int time , distance ;
+    cout << "enter time in hour = " ;
+    cin >> time ;
+    distance = 60 * time ;
+    cout << "total distance=" << distance ;
+    return 0 ;
+}
