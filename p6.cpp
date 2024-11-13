af55bc4e9937bc194a36b03b272ba4a1a002c73f
Maisum abbas <maisumabbas03@gmail.com>
2024-11-13 17:29:48 +0500

    p6

---
+++ p6.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	 int area;
+	cout<<"1.area of a circle\n 2.area of a rectangle\n 3.area of a triangle\n=";
+	switch(area)
+	{
+		case1:
+			{
+				int raduis;
+					cout<<"raduis of a circle:"<<endl;
+			cin>>raduis;
+			area=3.14*raduis*raduis;
+			cout<<"area of a circle ="<<area;
+			break;
+			}
+			case2:
+				{
+					int len, width;
+					cout<<"lenght and width of a rectangle:"<<endl;
+				cin>>len>>width;
+			
+				area=len*width;
+				cout<<"area of a rectangle="<<area;
+				break;
+				}
+				case3:
+					{
+						int base,height;
+						cout<<"base and height of a triangle:"<<endl;
+						cin>>base>>height;
+						area=0.5*base*height;
+						cout<<"area of a triangle is="<<area;
+						
+						break;
+                    }   
+						default:
+                        {
+							cout<<"quit:";
+                        }
+					
+	}
+	return 0;
+}
