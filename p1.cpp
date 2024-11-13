313c46957ff56e8155c9eff6bdc77e025fa066cb
Maisum abbas <maisumabbas03@gmail.com>
2024-11-13 15:49:52 +0500

    p1

---
+++ p1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace  std ;
+int main  ()
+{
+    int  c,f ;
+    cout << "enter temperature in celcius = \n" ;
+    cin  >> c ;
+    f =  (9/5)*c + 32 ;
+    cout << " temperatur   in   fahrenheit =  " << f ;
+    return 0 ;
+
+}  
