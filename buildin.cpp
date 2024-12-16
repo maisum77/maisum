ead1d8ba83a1d4951a7cc7b6afd5a2a5f53932c5
maisum <maisumabbas03@gmail.com>
2024-12-16 19:44:57 +0500

    sruare

---
+++ buildin.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std ;
+int main ()
+{
+    int a ;
+    double b ;
+    cout << "enter a number = " ;
+    cin>> a ;
+    b = sqrt(a) ;
+    cout << "square root of number is = " << b << endl ;
+    return 0 ;
+}
